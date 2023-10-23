f6e811e9801d7dd3f27074353d8b423809f10b14
Gaurav Lokhande <gaurravlokhande@gmail.com>
2023-10-23 17:30:48 +0530

    FC

--- force-app/main/default/lwc/eventsPage/eventsPage.js
+++ force-app/main/default/lwc/eventsPage/eventsPage.js
@@ -1,3 +1,38 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import dasra from '@salesforce/resourceUrl/dsra';
+import GetEventsData from '@salesforce/apex/SocietyManagementSystem.SearchEvents';
 
-export default class EventsPage extends LightningElement {}
+export default class EventsPage extends LightningElement {
+
+    @track dasra = dasra;
+
+
+    connectedCallback() {
+        this.Searchandshowevents();
+    }
+   
+
+    @track StoreEventData =[];
+
+    @track searchvalue = '';
+
+
+    Searchandshowevents() {
+        GetEventsData({ searchinit: this.searchvalue })
+        .then((result) => {
+            this.StoreEventData = result;
+            
+        }).catch((error) => {
+            this.StoreEventData = error;
+        });
+    
+    }
+   
+    onsearchvaluechange(event) {
+        this.searchvalue = event.target.value;
+        this.Searchandshowevents();
+    }
+    
+  
+    
+}
