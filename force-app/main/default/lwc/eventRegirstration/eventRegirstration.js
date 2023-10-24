03092d61ff9ed55ad58ea9c7319bc68032bc60b8
Gaurav Lokhande <gaurravlokhande@gmail.com>
2023-10-24 16:13:19 +0530

    SC

---
+++ force-app/main/default/lwc/eventRegirstration/eventRegirstration.js
@@ -0,0 +1,27 @@
+import { LightningElement, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+
+export default class EventRegirstration extends LightningElement {
+
+   
+    
+    handleRegister() {
+        this.dispatchEvent(new ShowToastEvent({
+            title: "Registered",
+            message: "Successfully Registered",
+            variant: "success"
+        }));
+    }
+
+    @track EventDataTemplate = false;
+    @track RegistrationTemplate = true;
+
+    handleCancel() {
+        this.RegistrationTemplate = false;
+        this.EventDataTemplate = true;
+    }
+}
+
+
+      
--- force-app/main/default/lwc/eventsPage/eventsPage.js
+++ force-app/main/default/lwc/eventsPage/eventsPage.js
@@ -1,38 +1,37 @@
 import { LightningElement, track, wire } from 'lwc';
-import dasra from '@salesforce/resourceUrl/dsra';
 import GetEventsData from '@salesforce/apex/SocietyManagementSystem.SearchEvents';
 
 export default class EventsPage extends LightningElement {
 
-    @track dasra = dasra;
+
+    @track EventPageTemplate = true;
+    @track registrationtamplate = false;
+    @track Registrationrequired = true;
+    @track StoreEventData = [];
+    @track searchvalue = '';
 
 
     connectedCallback() {
         this.Searchandshowevents();
     }
-   
-
-    @track StoreEventData =[];
-
-    @track searchvalue = '';
-
 
     Searchandshowevents() {
         GetEventsData({ searchinit: this.searchvalue })
-        .then((result) => {
-            this.StoreEventData = result;
-            
-        }).catch((error) => {
-            this.StoreEventData = error;
-        });
-    
+            .then((result) => {
+                this.StoreEventData = result;
+            })
+            .catch((error) => {
+                this.StoreEventData = error;
+            });
     }
-   
+
     onsearchvaluechange(event) {
         this.searchvalue = event.target.value;
         this.Searchandshowevents();
     }
-    
-  
-    
+
+    handleClickOfRegistrationButton() {
+        this.EventPageTemplate = false;
+        this.registrationtamplate = true;
+    }
 }
